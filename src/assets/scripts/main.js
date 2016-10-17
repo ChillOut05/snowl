27e116359f71c6c6c7cdbbef02d9b20568c05bc5
Vadim Solovei <chillout05@gmail.com>
2016-10-17 19:55:56 +0300

    added promo video bg

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,23 @@
-(()=>{
-	console.log('Hi there');
-})()
+// new MediaElement('#player', {
+	// startVolume: 0.8,
+	// loop: true,
+	// enableAutosize: true,
+	// alwaysShowControls: false,
+	// alwaysShowHours: false,
+	// showTimecodeFrameCount: false,
+// });
+// player.play();
+// new MediaElement('youtube', {
+// 	defaultVideoWidth: '100%',
+// 	defaultVideoHeight: 820,
+// 	startVolume: 0.0,
+// 	loop: true,
+// 	enableAutosize: true,
+// 	alwaysShowControls: false,
+// 	alwaysShowHours: false,
+// 	showTimecodeFrameCount: false,
+//
+// 	success: function(me) {
+// 		me.play();
+// 	}
+// })
