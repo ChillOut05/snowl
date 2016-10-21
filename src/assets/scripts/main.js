7e6f8b4da3b15f1f097207275d76158d40edb12b
Vadim Solovei <chillout05@gmail.com>
2016-10-21 20:36:20 +0300

    added bg video and product img animation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,23 +1,28 @@
-// new MediaElement('#player', {
-	// startVolume: 0.8,
-	// loop: true,
-	// enableAutosize: true,
-	// alwaysShowControls: false,
-	// alwaysShowHours: false,
-	// showTimecodeFrameCount: false,
-// });
-// player.play();
-// new MediaElement('youtube', {
-// 	defaultVideoWidth: '100%',
-// 	defaultVideoHeight: 820,
-// 	startVolume: 0.0,
-// 	loop: true,
-// 	enableAutosize: true,
-// 	alwaysShowControls: false,
-// 	alwaysShowHours: false,
-// 	showTimecodeFrameCount: false,
-//
-// 	success: function(me) {
-// 		me.play();
-// 	}
-// })
+$(document).ready(function() {
+	$('#video').YTPlayer({
+		fitToBackground: true,
+		videoId: '6fC__Sh1JEk'
+	});
+
+	$('.product__image').magnificPopup({
+		type: 'image',
+		mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+
+	});
+});
